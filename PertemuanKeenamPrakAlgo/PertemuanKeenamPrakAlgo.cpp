7e79690142ef7452c7f394940611a8eacf2f044a
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-03-29 10:41:11 +0700

    Memasukan kondisi do while

--- PertemuanKeenamPrakAlgo/PertemuanKeenamPrakAlgo.cpp
+++ PertemuanKeenamPrakAlgo/PertemuanKeenamPrakAlgo.cpp
@@ -94,5 +94,14 @@ int main()
 {
 	char ch;
 
-	do 
+	do {
+		input();
+		q_short(0, n - 1);
+		display();
+		cout << "\n\nDo you want to continue? (y/n): ";
+		cin >> ch;
+		if (ch == 'n' || ch == 'N')
+			break;
+
+	}
 }
